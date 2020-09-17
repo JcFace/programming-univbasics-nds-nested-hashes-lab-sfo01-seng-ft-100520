feeebff519121415bb585751952e95a625b7dec0
Jesse Vaughn <jcv221@gmail.com>
2020-09-17 21:43:07 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -38,7 +38,7 @@ aoh[2][:name]
 end
 
 def aoh_update(aoh, row, key, new_value)
-aoh[row][key] = new_value
+aoh[row][key] = "new_value"
 end
 
  # Update the AoH data at row and key to have the value of new_value
